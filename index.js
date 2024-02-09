d03709112c897f8db5347607d986c77e40326e5b
Tushar <tushard3998@gmail.com>
2024-02-09 12:50:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+console.log(" JavaScript Strings ")
+
+let a = "Tushar";
+
+console.log(a)
+
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+
+//LENGTH OF AN ARRAY
+
+console.log(a.length)
+
+let name = "Tushar";
+let friend = "ayush";
+
+console.log("His Name Is " + name + "And His Friend Name Is" + friend);
+
+//TEMPLATE LITERALS
+
+console.log(`His Name Is ${name } and His Friend Name Is ${friend} ` )
+
+let b = "Shivam";
+
+//FUNCTIONS
+
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.slice(2))
+console.log(b.slice(2,4))
+console.log(b.replace("sh" ,"tu"))
+console.log(b.concat(a,"Aishwariya", "Rahul" , "Priya"))
